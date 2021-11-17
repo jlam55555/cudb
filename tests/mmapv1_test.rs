ecd0c621998651ae4ace0be7c1074f51a68d5590
Jonathan Lam <jlam55555@gmail.com>
2021-11-17 02:10:28 -0500

    Fix unit tests for `alloc_size` using new header scheme

--- tests/mmapv1_test.rs
+++ tests/mmapv1_test.rs
@@ -13,19 +13,24 @@ pub mod tests {
         assert_eq!(alloc_size(1), MIN_BLOCK_SIZE);
         assert_eq!(alloc_size(2), MIN_BLOCK_SIZE);
         assert_eq!(alloc_size(3), MIN_BLOCK_SIZE);
-        assert_eq!(alloc_size(MIN_BLOCK_SIZE), MIN_BLOCK_SIZE);
+        assert_eq!(alloc_size(MIN_BLOCK_SIZE - HEADER_SIZE - 1), MIN_BLOCK_SIZE);
+        assert_eq!(alloc_size(MIN_BLOCK_SIZE - HEADER_SIZE), MIN_BLOCK_SIZE);
+        assert_eq!(
+            alloc_size(MIN_BLOCK_SIZE - HEADER_SIZE + 1),
+            2 * MIN_BLOCK_SIZE
+        );
         assert_eq!(alloc_size(17), 32);
         assert_eq!(alloc_size(43), 64);
         assert_eq!(alloc_size(23123), 32768);
         assert_eq!(alloc_size(1000000), 1048576);
-        assert_eq!(alloc_size(MAX_BLOCK_SIZE), MAX_BLOCK_SIZE);
+        assert_eq!(alloc_size(MAX_BLOCK_SIZE - HEADER_SIZE), MAX_BLOCK_SIZE);
     }
 
     // Testing that allocating past `MAX_BLOCK_SIZE` should panic.
     #[test]
     #[should_panic(expected = "document >1MB")]
     fn test_invalid_alloc_size() {
-        alloc_size(MAX_BLOCK_SIZE + 1);
+        alloc_size(MAX_BLOCK_SIZE - HEADER_SIZE + 1);
     }
 
     // Testing pool creation/deletion
