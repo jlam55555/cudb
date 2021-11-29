d67ca8831c25f3175ca25fdae2e69af7c4380067
DerekLeeCS <51767748+DerekLeeCS@users.noreply.github.com>
2021-11-28 20:25:23 -0500

    Update code style

--- build_docs.sh
+++ build_docs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# build documentation
+# Build documentation
 cargo doc --no-deps
 
-# move from default docs location to github pages location
+# Move from default docs location to github pages location
 rm -rf docs/
 cp -r target/doc/ docs/
 
-# build default landing/redirect page at document root
+# Build default landing/redirect page at document root
 echo "<script>document.location.href='cudb/index.html';</script>" >docs/index.html
