ee4d5ca54f19df80f37e2e07ff186abc01b2c799
Jonathan Lam <jlam55555@gmail.com>
2021-11-30 16:49:04 -0500

    Rebuild documentation

--- docs/implementors/core/hash/trait.Hash.js
+++ docs/implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["cudb"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"cudb/index/struct.Index.html\" title=\"struct cudb::index::Index\">Index</a>","synthetic":false,"types":["cudb::index::Index"]}];
+implementors["cudb"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"cudb/index/struct.IndexSchema.html\" title=\"struct cudb::index::IndexSchema\">IndexSchema</a>","synthetic":false,"types":["cudb::index::IndexSchema"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
