2ecc597ef8034a15c93a3786b9d28955435710eb
Jonathan Lam <jlam55555@gmail.com>
2021-12-14 14:28:14 -0500

    Move main.rs to examples/

--- examples/index_perf.rs
+++ examples/index_perf.rs
@@ -9,7 +9,6 @@ use rand::Rng;
 use std::collections::HashMap;
 use std::time::Instant;
 
-#[allow(dead_code)]
 fn populate_collection(col: &mut Collection, n: i32) {
     println!("Populating database...");
     let start = Instant::now();
@@ -30,7 +29,6 @@ fn populate_collection(col: &mut Collection, n: i32) {
     println!("Elapsed: {}ms", start.elapsed().as_millis());
 }
 
-#[allow(dead_code)]
 fn print_collection_size(col: &Collection) {
     println!("Counting documents...");
     let start = Instant::now();
@@ -40,17 +38,6 @@ fn print_collection_size(col: &Collection) {
     println!("Elapsed: {}ms", start.elapsed().as_millis());
 }
 
-#[allow(dead_code)]
-fn drop_collection(col: Collection) {
-    println!("Dropping collection...");
-    let start = Instant::now();
-
-    col.drop();
-
-    println!("Elapsed: {}ms", start.elapsed().as_millis());
-}
-
-#[allow(dead_code)]
 fn create_index(col: &mut Collection) {
     println!("Creating index...");
     let start = Instant::now();
@@ -63,7 +50,6 @@ fn create_index(col: &mut Collection) {
     println!("Elapsed: {}ms", start.elapsed().as_millis());
 }
 
-#[allow(dead_code)]
 fn query_lt_100(col: &Collection) {
     println!("Querying elements 0 < x < 100...");
     let start = Instant::now();
@@ -99,18 +85,19 @@ fn query_lt_100(col: &Collection) {
     println!("Elapsed: {}ms", start.elapsed().as_millis());
 }
 
-#[allow(unused_variables, unused_mut)]
 fn main() {
-    // Create/open collection
+    // Remove old collection
+    Collection::from("my_database.db").drop();
+
+    // Create and populate collection
     let mut col = Collection::from("my_database.db");
+    populate_collection(&mut col, 100000);
+    print_collection_size(&col);
 
-    // drop_collection(col);
-
-    // populate_collection(&mut col, 100000);
-    // print_collection_size(&col);
-
-    // create_index(&mut col);
     query_lt_100(&col);
 
-    // col.close();
+    create_index(&mut col);
+    query_lt_100(&col);
+
+    col.close();
 }
