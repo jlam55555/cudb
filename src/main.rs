295cb029da21f55289822a4f0e6dbed79df91d91
Jonathan Lam <jlam55555@gmail.com>
2021-12-14 08:06:11 -0500

    Add main example to show the performance benefit of indexing

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,98 @@
+use cudb::db::Collection;
 use cudb::document::Document;
+use cudb::index::FieldSpec;
+use cudb::query::{Constraint, Query};
 use cudb::value::Value;
 
+use rand::Rng;
+
+use std::collections::HashMap;
+use std::time::Instant;
+
+#[allow(dead_code)]
+fn populate_collection(col: &mut Collection, n: i32) {
+    println!("Populating database...");
+    let start = Instant::now();
+
+    let mut docs = Vec::new();
+    let mut rng = rand::thread_rng();
+    for _ in 0..n {
+        docs.push(Document::from(HashMap::from([(
+            String::from("randid"),
+            Value::Int32(rng.gen::<i32>() % 1000),
+        )])));
+    }
+    col.insert_many(docs);
+
+    println!("Elapsed: {}ms", start.elapsed().as_millis());
+}
+
+#[allow(dead_code)]
+fn print_collection_size(col: &Collection) {
+    println!("Counting documents...");
+    let start = Instant::now();
+
+    println!("Number of documents: {}", col.find_all().len());
+
+    println!("Elapsed: {}ms", start.elapsed().as_millis());
+}
+
+#[allow(dead_code)]
+fn drop_collection(col: Collection) {
+    println!("Dropping collection...");
+    let start = Instant::now();
+
+    col.drop();
+
+    println!("Elapsed: {}ms", start.elapsed().as_millis());
+}
+
+#[allow(dead_code)]
+fn create_index(col: &mut Collection) {
+    println!("Creating index...");
+    let start = Instant::now();
+
+    col.declare_index(vec![FieldSpec::new(
+        vec![String::from("randid")],
+        Value::Int32(0),
+    )]);
+
+    println!("Elapsed: {}ms", start.elapsed().as_millis());
+}
+
+#[allow(dead_code)]
+fn query_lt_100(col: &Collection) {
+    println!("Querying elements 0 < x < 100...");
+    let start = Instant::now();
+
+    let query = Query {
+        constraints: HashMap::from([(
+            vec![String::from("randid")],
+            Constraint::And(
+                Box::new(Constraint::LessThan(Value::Int32(100))),
+                Box::new(Constraint::GreaterThan(Value::Int32(0))),
+            ),
+        )]),
+        projection: HashMap::new(),
+        order: None,
+    };
+
+    let query_results = col.find_many(query);
+    println!("Number of results: {}", query_results.len());
+
+    println!("Elapsed: {}ms", start.elapsed().as_millis());
+}
+
+#[allow(unused_variables, unused_mut)]
 fn main() {
-    // Create a new mutable document
-    let mut d = Document::new();
+    // Create/open collection
+    let mut col = Collection::from("my_database.db");
 
-    // Insert some values into the document
-    d.insert(
-        "Hello".to_string(),
-        Value::Id("12312312541243123".to_string()),
-    );
+    // drop_collection(col);
 
-    // Print the document using the default debug trait
-    println!("d = {:?}", d);
+    // populate_collection(&mut col, 100000);
+    // print_collection_size(&col);
 
-    // bincode serialization
-    let encoded = bincode::serialize(&d).unwrap();
-    println!("encoded = {:?}", encoded);
-
-    let decoded: Document = bincode::deserialize(&encoded[..]).unwrap();
-    println!("decoded = {:?}", decoded);
+    // create_index(&mut col);
+    // query_lt_100(&col);
 }
