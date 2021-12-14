10949e044ff279735fb56c2d3867ae27364dac5c
Jonathan Lam <jlam55555@gmail.com>
2021-12-14 06:39:10 -0500

    Add test for update document function

--- tests/value_test.rs
+++ tests/value_test.rs
@@ -7,9 +7,9 @@ mod utils;
 
 #[cfg(test)]
 pub mod tests {
-    use std::collections::HashMap;
-    use cudb::document::Document;
     use super::*;
+    use cudb::document::Document;
+    use std::collections::HashMap;
 
     // Test each variant in the enum.
     #[test]
@@ -44,17 +44,14 @@ pub mod tests {
 
     #[test]
     fn test_is_variant_equal_dict() {
-        let val_dict_a = Value::Dict(Document::from(
-            HashMap::from([
-                ("key".to_string(), Value::Int32(1)),
-            ]))
-        );
-        let val_dict_b = Value::Dict(Document::from(
-            HashMap::from([
-                ("abc".to_string(), Value::String("xyz".to_string())),
-                ("temp".to_string(), Value::Id("12345".to_string())),
-            ])
-        ));
+        let val_dict_a = Value::Dict(Document::from(HashMap::from([(
+            "key".to_string(),
+            Value::Int32(1),
+        )])));
+        let val_dict_b = Value::Dict(Document::from(HashMap::from([
+            ("abc".to_string(), Value::String("xyz".to_string())),
+            ("temp".to_string(), Value::Id("12345".to_string())),
+        ])));
         let val_array = Value::Array(Vec::from([
             Value::Int32(5),
             Value::String("abc".to_string()),
@@ -70,13 +67,63 @@ pub mod tests {
             Value::Int32(5),
             Value::String("abc".to_string()),
         ]));
-        let val_array_b = Value::Array(Vec::from([
-            Value::Id("67890".to_string()),
-            Value::Int32(2),
-        ]));
+        let val_array_b =
+            Value::Array(Vec::from([Value::Id("67890".to_string()), Value::Int32(2)]));
         let val_str = Value::String("temp".to_string());
 
         assert!(val_array_a.is_variant_equal(&val_array_b));
         assert!(!val_array_a.is_variant_equal(&val_str));
+    }
+
+    // Test that update document functionality works.
+    #[test]
+    fn test_update_from_document() {
+        let mut doc = Document::new();
+        doc.insert(String::from("a"), Value::String(String::from("hello")));
+        doc.insert(String::from("b"), Value::String(String::from("world")));
+        let mut subdoc = Document::new();
+        subdoc.insert(String::from("d"), Value::Int32(4));
+        doc.insert(String::from("c"), Value::Dict(subdoc));
+
+        let update_doc = Document::from(HashMap::from([
+            (String::from("a"), Value::String(String::from("new value"))),
+            (String::from("e"), Value::Int32(55)),
+            (
+                String::from("c"),
+                Value::Dict(Document::from(HashMap::from([
+                    (String::from("d"), Value::String(String::from("updated d"))),
+                    (
+                        String::from("f"),
+                        Value::Dict(Document::from(HashMap::from([(
+                            String::from("g"),
+                            Value::Int32(-2),
+                        )]))),
+                    ),
+                ]))),
+            ),
+        ]));
+
+        doc.update_from(&update_doc);
+
+        assert!(
+            doc == Document::from(HashMap::from([
+                (String::from("a"), Value::String(String::from("new value"))),
+                (String::from("b"), Value::String(String::from("world"))),
+                (String::from("e"), Value::Int32(55)),
+                (
+                    String::from("c"),
+                    Value::Dict(Document::from(HashMap::from([
+                        (String::from("d"), Value::String(String::from("updated d"))),
+                        (
+                            String::from("f"),
+                            Value::Dict(Document::from(HashMap::from([(
+                                String::from("g"),
+                                Value::Int32(-2),
+                            )]))),
+                        ),
+                    ]))),
+                ),
+            ]))
+        );
     }
 }
